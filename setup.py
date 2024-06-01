bf630a97adab73c2a4119ee4d5e19250a8960483
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2024-06-02 00:57:08 +0500

    fix bugs

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='log_writer',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     include_package_data=True,
     author='Zohidillo Turgunov',
+    license="MIT",
     author_email='zohidilloturgunov@mail.ru',
     description='A simple logging package, Loglarni filega yozib borish uchun kichik packege',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url='https://github.com/ZohidilloPr/log_writer/tree/main',
     classifiers=[
         'Programming Language :: Python :: 3',
