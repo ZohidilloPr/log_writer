3439858ec025d42cc7e51177e7b8cbf4897afa6d
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2024-06-01 22:59:21 +0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='writer_log',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    author='Zohidillo Turgunov',
+    author_email='zohidilloturgunov@mail.ru',
+    description='A simple logging package, Loglarni filega yozib borish uchun kichik packege',
+)
